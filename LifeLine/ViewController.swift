b80280a789e79231f9b521218bc97d672456f87d
‚ÄúKEEVIN <Aukeevin.mitchell@gmail.comAu>
2015-10-12 22:29:56 -0400

    Initial Commit

---
+++ LifeLine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LifeLine
+//
+//  Created by KEEVIN MITCHELL on 10/12/15.
+//  Copyright © 2015 Beyond 2021. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
